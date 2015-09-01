a74c21be297ea8a73fe66f00c0c4fa1e45aaafe0
rrgayhart <rrgayhart@gmail.com>
2015-09-01 09:50:19 -0600

    Add text reading helper for specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,7 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rspec/rails'
 
 #ActiveRecord::Migration.maintain_test_schema!
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
 RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
