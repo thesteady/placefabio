8f7ef63850cf17a8f257ea265de3ee5feaba23ca
rrgayhart <rrgayhart@gmail.com>
2015-09-13 10:08:52 -0600

    Refactor and test ipsum fodder js

--- app/assets/javascripts/ipsum-fodder.js
+++ app/assets/javascripts/ipsum-fodder.js
@@ -1,40 +1,49 @@
+var IpsumFodder = {
+  cleanWordParams: function(word) {
+    return word.replace('word-', '');
+  },
+  cleanCategoryParams: function(category) {
+    return category.replace('category-', '');
+  },
+  handleIgnoreParam: function(categoryType, wordParams) {
+    if (categoryType == 'ignore'){
+      wordParams.ignore = true;
+    }
+    return wordParams;
+  },
+  postNewWord: function(wordParams, wordList, wordId) {
+    $.ajax({
+      type: "POST",
+      url: "/words",
+      data: {word: wordParams},
+      success: function(data) {
+        $(wordList).prepend(document.getElementById(wordId));
+      },
+      error: function(data) {
+        alert(data.responseText.toString());
+      }
+    });
+  },
+  dropNewWord: function(ev) {
+    ev.preventDefault();
+    //TODO: Show 'upload more' when no more words present
+    var evWordId = ev.dataTransfer.getData("text");
+    var evCategoryId = event.currentTarget.id;
+    var wordList = $(event.currentTarget).find('ul')[0];
+
+    var wordName = IpsumFodder.cleanWordParams(evWordId);
+    var categoryType = IpsumFodder.cleanCategoryParams(evCategoryId);
+    var baseWordParams = {name: wordName, category: categoryType};
+    var wordParams = IpsumFodder.handleIgnoreParam(categoryType, baseWordParams);
+
+    IpsumFodder.postNewWord(wordParams, wordList, evWordId);
+  }
+};
+
 function allowDrop(ev) {
   ev.preventDefault();
 }
 
 function drag(ev) {
   ev.dataTransfer.setData("text", ev.target.id);
-}
-
-function postNewWord(wordParams, wordList, wordId) {
-  $.ajax({
-    type: "POST",
-    url: "/words",
-    data: {word: wordParams },
-    success: function(data) {
-      $(wordList).prepend(document.getElementById(wordId));
-    },
-    error: function(data) {
-      alert(data.responseText.toString());
-    }
-  });
-}
-
-function dropNewWord(ev) {
-  ev.preventDefault();
-  //TODO: Disallow dragging on non-category elements
-  var evWordId = ev.dataTransfer.getData("text");
-  var evCategoryId = ev.target.id;
-  var wordList = $(ev.target).find('ul')[0];
-
-  var wordName = evWordId.replace('word-', '');
-  var categoryType = evCategoryId.replace('category-', '');
-
-  var wordParams = {name: wordName, category: categoryType};
-
-  if (categoryType == 'ignore'){
-    wordParams.ignore = true;
-  }
-
-  postNewWord(wordParams, wordList, evWordId);
 }
